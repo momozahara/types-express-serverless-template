ded2a9e5c940a32b603b9efd3147a7c3e8647ca5
Pream Pinbut <momozahara@hotmail.com>
2022-05-19 02:40:06 +0700

    replace tab with space

--- src/routes/main.ts
+++ src/routes/main.ts
@@ -3,11 +3,11 @@ import express, { Router, Request, Response } from 'express';
 const router: Router = express.Router();
 
 router.get('/', (req: Request, res: Response) => {
-	res.status(200).json({
-		message: 'Express + TypeScript'
-	});
+  res.status(200).json({
+    message: 'Express + TypeScript'
+  });
 });
 
 export {
-	router
+  router
 };
