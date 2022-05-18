50ec982bd568cf33ebeb7909bcfd3a13abb00785
Pream Pinbut <momozahara@hotmail.com>
2022-05-19 03:43:35 +0700

    remove unused variable

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,4 @@
-import express, { Express, Request, Response } from 'express';
+import express, { Express } from 'express';
 import ServerlessHttp from 'serverless-http';
 import cors from 'cors';
 
